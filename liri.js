ceec54116175d9753cda044de734f84da8fe6f5b
Robert Shaw <robertshaw87@gmail.com>
2018-05-18 14:54:19 -0700

    initialize files

---
+++ liri.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var inquirer = require('inquirer');
+var request = require('request');
+
+console.log("sup")
