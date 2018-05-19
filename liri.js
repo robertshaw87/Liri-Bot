bccde16cfb5247700ff05e0bc24134051d021df3
Robert Shaw <robertshaw87@gmail.com>
2018-05-19 13:43:16 -0700

    add movie functionality

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-var keys = require("keys");
+var keys = require("./keys.js");
 // var weather = require("weather-js");
 var TwitterReq = require('twitter');
 var SpotifyReq = require('node-spotify-api');
@@ -9,10 +9,39 @@ var request = require('request');
 var spotify = new SpotifyReq(keys.spotify);
 var twitter = new TwitterReq(keys.twitter);
 
-function UserChoose() {
+start();
+
+function start() {
+    console.log("\n==============================\n  Hello! My name is Liri. \n==============================\n");
+    var userInput = process.argv;
+    userInput.splice(0, 2);
+    if (userInput.length === 0) {
+        userChoose();
+    } else {
+        console.log("COMMAND LINE ARGUMENTS");
+    }
+}
+
+function pause(callback) {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please press ENTER to continue.",
+            name: "userInput",
+        }
+    ]).then(callback);
+}
+
+function end() {
+    console.log("\n==============================\n  Goodbye\n==============================\n");
+}
+
+function userChoose() {
+    console.log("\n")
     inquirer.prompt([
         {
             type: "list",
+            message: "How can I help you today?",
             choices: ["Check out my Tweets", "Spotify a song", "Look up a movie", "Do what it says", "Nothing for now"],
             name: "userChoice"
         }
@@ -30,6 +59,9 @@ function UserChoose() {
             case "Do what it says":
                 chooseText();
                 break;
+            default:
+                end();
+                break;
         }
     })
 }
@@ -43,7 +75,47 @@ function chooseSpotify() {
 }
 
 function chooseMovie() {
-    console.log("Movie")
+    console.log("\n");
+    inquirer.prompt({
+        type: "input",
+        message: "Which movie would you like for me to look up?",
+        name: "userMovie",
+    }).then((response) => {
+        if (response.userMovie){
+            lookUpMovie(response.userMovie);
+        } else {
+            lookUpMovie("Mr. Nobody");
+        }
+    })
+}
+
+function lookUpMovie(movieName) {
+    var requestName = movieName.replace(" ", "+");
+    var requestURL = "http://www.omdbapi.com/?t=" + requestName + "&y=&plot=short&apikey=trilogy";
+    request(requestURL, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            displayMovie(JSON.parse(response.body));
+            pause(userChoose);
+        } else {
+            console.log(error)
+        }
+    });
+}
+
+function displayMovie (movieObj) {
+    console.log(movieObj)
+    console.log("\n==============================\n");
+    console.log(movieObj.Title);
+    console.log(movieObj.Released);
+    console.log("");
+    console.log("Country: " + movieObj.Country);
+    console.log("Language: " + movieObj.Language);
+    console.log("Starring: " + movieObj.Actors);
+    console.log("Plot: " + movieObj.Plot);
+    console.log("");
+    console.log("IMDB: " + movieObj.imdbRating);
+    console.log("Rotten Tomatoes: ");
+
 }
 
 function chooseText() {
